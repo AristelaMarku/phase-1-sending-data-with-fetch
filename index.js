ef25e2f4a700648a59c065b88e3ecd6f17478b25
Aristela Marku <aristela.marku@gmail.com>
2021-10-01 16:02:50 +0200

    commit tree a441b1b771872769d349190d60389343fa00d6ce

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Add your code here
+
+function submitData(newName,newEmail){
+
+   
+    return fetch('http://localhost:3000/users',{
+        method:'POST',
+        headers:{
+            "Content-Type": "application/json",
+             "Accept":"application/json",
+        },
+        body: JSON.stringify({
+            name: newName,
+            email: newEmail,
+          }),
+    })
+    .then(res=>res.json())
+
+    .then(object=>{
+        console.log(object)
+        const body=document.querySelector('body')
+        body.innerHTML=object.id
+
+    })
+    .catch(error=>{
+        console.log(error)
+        const body=document.querySelector('body')
+        const p=document.createElement('p')
+        p.innerHTML=error.message
+        body.append(p)
+
+    })
+}
+submitData()
